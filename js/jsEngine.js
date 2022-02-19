33541fca896dcd7c4e49e9e951fe6f21a34c9c6d
sagar <sagarsurendhrababau@gmail.com>
2022-02-19 19:13:59 +0530

    product gallery custom javascript updated

---
+++ js/jsEngine.js
@@ -0,0 +1,66 @@
+// product gallery sections
+(function(){    
+
+    const productData = [
+        {
+            id:1,
+            image:'./assets/img/products/image 01.jpg',
+            thumb:'./assets/img/products/temp01.jpg'
+        },
+        {
+            id:2,
+            image:'./assets/img/products/image 02.jpg',
+            thumb:'./assets/img/products/temp02.jpg'
+        },
+        {
+            id:3,
+            image:'./assets/img/products/image 03.jpg',
+            thumb:'./assets/img/products/temp03.jpg'
+        },
+        {
+            id:4,
+            image:'./assets/img/products/image 04.jpg',
+            thumb:'./assets/img/products/temp04.jpg'
+        },
+        {
+            id:5,
+            image:'./assets/img/products/image 05.jpg',
+            thumb:'./assets/img/products/temp05.jpg'
+        },
+        {
+            id:6,
+            image:'./assets/img/products/image 06.jpg',
+            thumb:'./assets/img/products/temp06.jpg'
+        },
+        {
+            id:7,
+            image:'./assets/img/products/image 07.jpg',
+            thumb:'./assets/img/products/temp07.jpg'
+        },
+        {
+            id:8,
+            image:'./assets/img/products/image 08.jpg',
+            thumb:'./assets/img/products/temp08.jpg'
+        },                                                        
+    ];
+
+    const productGalleryCanvas = document.querySelector(".productGalleryCanvas");
+    const productDisplay = document.querySelector(".productDisplay img")
+
+    console.log(productDisplay);
+
+    productData.forEach(el => {
+        const li = document.createElement("li");
+        const img = document.createElement("img");
+        productGalleryCanvas.append(li);
+        li.appendChild(img);
+        img.setAttribute("src", el.thumb);
+
+        li.addEventListener("click", ()=>{
+            productDisplay.setAttribute("src",el.image)
+        })
+
+    })
+
+
+})();
